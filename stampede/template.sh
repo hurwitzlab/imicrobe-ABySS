c58a3163936958a2e14f909f66e66b9665a3607d
Joshua Lynch <jklynch@email.arizona.edu>
2017-10-20 14:34:59 -0400

    add missing parameters

--- stampede/template.sh
+++ stampede/template.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo "Started $(date)"
+echo "Started iMicrobe ABySS template.sh $(date)"
 
-echo "SINGLE_END_READS        \"${SINGLE_END_READS}\""
+echo "NAME                    \"${NAME}\""
+echo "KMER_LENGTH             \"${KMER_LENGTH}\""
 echo "FORWARD_READS           \"${FORWARD_READS}\""
 echo "REVERSE_READS           \"${REVERSE_READS}\""
-echo "NAME                    \"${NAME}\""
-echo "k                       \"${k}\""
+echo "SINGLE_END_READS        \"${SINGLE_END_READS}\""
+echo "FORWARD_MATE_PAIRS      \"${FORWARD_MATE_PAIRS}\""
+echo "REVERSE_MATE_PAIRS      \"${REVERSE_MATE_PAIRS}\""
+echo "LONG_SEQS               \"${LONG_SEQS}\""
 echo "ADDITIONAL_ARGS         \"${ADDITIONAL_ARGS}\""
 
-mkdir abyss-out
+##mkdir abyss-out
 
-sh run.sh \
-    ${NAME} \
-    ${k} \
-    ${FORWARD_READS} ${REVERSE_READS} ${SINGLE_END_READS}
+time sh run.sh \
+    ${NAME} ${KMER_LENGTH} ${FORWARD_READS} ${REVERSE_READS} ${SINGLE_END_READS} ${FORWARD_MATE_PAIRS} ${REVERSE_MATE_PAIRS} ${LONG_SEQS} ${ADDITIONAL_ARGS}
 
-echo "Ended $(date)"
+echo "Ended iMicrobe ABySS template.sh $(date)"
 exit 0
